f026ea2f5b8cf0caba82d2e727ef312c692ea99e
nswisa <nivswisa9@gmail.com>
2022-04-05 18:17:57 +0300

    build faild

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.css';
 import ReactRouter from "./Router/routes";
-import "@fontsource/roboto/400.css";
 import Container from "@mui/material/Container";
 
 function App() {
